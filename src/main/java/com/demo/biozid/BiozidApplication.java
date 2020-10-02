997e3e4c81e8c7cc31ac025215986b62cb588004
Biozid Bostami <biozid0208@gmail.com>
2020-10-02 20:29:07 +0600

    [Initial] Project structure initiated

---
+++ src/main/java/com/demo/biozid/BiozidApplication.java
@@ -0,0 +1,13 @@
+package com.demo.biozid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiozidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiozidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/biozid/BiozidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.biozid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiozidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
