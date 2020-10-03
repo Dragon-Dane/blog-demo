9b9bbe81e4d19719b1a14fc6a5580e2166943e32
Biozid Bostami <biozid0208@gmail.com>
2020-10-03 15:50:21 +0600

    [Add] Spring security and registration view with boot strap

---
+++ src/main/java/com/demo/biozid/dtos/UserRegistrationDto.java
@@ -0,0 +1,39 @@
+package com.demo.biozid.dtos;
+
+import com.demo.biozid.annotations.FieldMatch;
+import lombok.Data;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@FieldMatch.List({
+        @FieldMatch(first = "password", second = "confirmPassword", message = "The password fields must match"),
+        @FieldMatch(first = "email", second = "confirmEmail", message = "The email fields must match")
+})
+
+@Data
+public class UserRegistrationDto {
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @NotEmpty
+    private String password;
+
+    @NotEmpty
+    private String confirmPassword;
+
+    @Email
+    @NotEmpty
+    private String email;
+
+    @Email
+    @NotEmpty
+    private String confirmEmail;
+
+    @AssertTrue
+    private Boolean terms;
+}
